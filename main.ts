ffe792ddb63866314243859143d498955068f1b9
hogan96 <234513426@qq.com>
2022-07-13 17:16:41 +0800

    更改描述

--- main.ts
+++ main.ts
@@ -11,29 +11,29 @@ let K=0
 let NTU=0
 let x=0
     export enum temppin {
-           //% block=pin0
-           pin0 = 0,
-           //% block=pin1
-           pin1 = 1,
-           //% block=pin2
-           pin2 = 2,
-           //% block=pin5
-           pin5 = 5,
-           //% block=pin8
-           pin8 = 8,
-           //% block=pin11
-           pin11 = 11,
-           //% block=pin12
-           pin12 = 12,
-           //% block=pin13
-           pin13 = 13,
-           //% block=pin14
-           pin14 = 14,
-           //% block=pin15
-           pin15 = 15,
-           //% block=pin16
-           pin16 = 16
-         }
+        //% block=pin0
+        pin0 = 0,
+        //% block=pin1
+        pin1 = 1,
+        //% block=pin2
+        pin2 = 2,
+        //% block=pin5
+        pin5 = 5,
+        //% block=pin8
+        pin8 = 8,
+        //% block=pin11
+        pin11 = 11,
+        //% block=pin12
+        pin12 = 12,
+        //% block=pin13
+        pin13 = 13,
+        //% block=pin14
+        pin14 = 14,
+        //% block=pin15
+        pin15 = 15,
+        //% block=pin16
+        pin16 = 16
+        }
 
     //% blockId=ds18init block="Init Water Temp Pin %pin"
     //% group="Water Temperature Sensor" weight=100
