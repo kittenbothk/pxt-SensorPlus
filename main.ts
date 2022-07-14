ee589e4f7080a8fcdf77633cb2b02149da107809
kittenbothk <kittenbotdummy@gmail.com>
2022-07-14 16:11:43 +0800

    change to modulesPlus, try gps

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 // SensorPlus@KittenBotHK
 
 //% color="#76dbb1" weight=10 icon="\uf2ce"
-//% groups='["SensorPlus"]'
-namespace SensorPlus {
+//% groups='["ModulesPlus"]'
+namespace ModulesPlus {
 let temp=25
 let deltaU=0
 let U=0
@@ -10,6 +10,13 @@ let U25=0
 let K=0
 let NTU=0
 let x=0
+
+let SerialData: Buffer = null
+let SerialString=""
+let serialBuf: string[] = []
+let UTC=""
+let longitude=""
+let latitude=""
     export enum temppin {
         //% block=P0
         P0 = 0,
@@ -102,4 +109,86 @@ let x=0
     export function lens_set(pin: DigitalPin, state: lensState){
         pins.digitalWritePin(pin,state)
     }
+
+    export enum timeindex{
+    //% block="Hour"
+    hour=0,
+    //% block="Minute"
+    min=1,
+    //% block="Second"
+    sec=2,
+    }
+    /**
+       * init serial port
+       * @param tx Tx pin; eg: SerialPin.P1
+       * @param rx Rx pin; eg: SerialPin.P2
+       */
+      //% blockId=gps_init block="GPS init|Tx(Blue) pin %tx|Rx(Green) pin %rx"
+      //% group="Basic" weight=100
+      export function gps_init(tx: SerialPin, rx: SerialPin): void {
+        serial.redirect(tx, rx, BaudRate.BaudRate9600)
+        serial.setRxBufferSize(72)
+        basic.pause(100)
+      }
+
+      //% blockId=gps_read block="GPS Read Data"
+      //% group="Basic" weight=95
+      export function gps_read(){
+      SerialString=''
+      while (!SerialString.includes("GNGGA")){
+        SerialData=serial.readBuffer(72)
+        for (let i =0; i<=SerialData.length;i++){
+          let temp = String.fromCharCode(SerialData[i])
+          SerialString = SerialString + temp
+        }
+        basic.pause(100)
+      }
+      if (SerialString.includes("GNGGA")){
+        serialBuf=SerialString.split(",")
+      } else {
+        serialBuf=[]
+      }
+
+      if (serialBuf.length>=4){
+        UTC=serialBuf[1]
+        latitude=serialBuf[2]
+        longitude=serialBuf[4]
+      }
+      }
+
+      //% blockId=gps_utc block="GPS get UTC Time %i"
+      //% group="Basic" weight=85
+      export function gps_utc(i:timeindex): number{
+      if (UTC!=''){
+      let time=[]
+      time[0]=parseFloat(UTC.substr(0,2))
+      time[0]=time[0]+8
+      time[1]=parseFloat(UTC.substr(2,2))
+      time[2]=parseFloat(UTC.substr(4,2))
+      return time[i]
+      }
+      else return 0
+      }
+
+      //% blockID=gps_latitude block="GPS Get Latitude"
+      //% group="Basic" weight=80
+      export function gps_latitude():number{
+      let latfinal = -1
+      if (latitude!=''){
+      latfinal=parseFloat(latitude.substr(0,2))+(parseFloat(latitude.substr(2,latitude.length)))/60
+      latfinal= parseFloat((convertToText(latfinal).substr(0,8)))
+      }
+      return latfinal
+      }
+
+      //% blockID=gps_longitude block="GPS Get Longitude"
+      //% group="Basic" weight=75
+      export function gps_longitude():number{
+      let lonfinal = -1
+      if (longitude!=''){
+      lonfinal=parseFloat(longitude.substr(0,3))+(parseFloat(longitude.substr(3,longitude.length)))/60
+      lonfinal= parseFloat((convertToText(lonfinal).substr(0,8)))
+      }
+      return lonfinal
+      }
 }
