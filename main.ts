a208020ab7a2fe00396819dbc667608bc649bc4b
kittenbothk <kittenbotdummy@gmail.com>
2022-07-19 12:06:59 +0800

    try new ds18 ext

--- main.ts
+++ main.ts
@@ -17,6 +17,16 @@ let serialBuf: string[] = []
 let UTC=""
 let longitude=""
 let latitude=""
+
+//% whenUsed
+let errorHandler:Action = null;
+//% whenUsed
+let errorObjectIdx : number = 0;
+//% whenUsed
+let errorPort : number = -1;
+// TODO: Localization
+const errorMsgs  = [ "No Error", "Not Connected", "Start Error", "Read Timeout", "Conversion Failure"];
+
     export enum temppin {
         //% block=P0
         P0 = 0,
@@ -191,4 +201,48 @@ let latitude=""
       }
       return lonfinal
       }
+
+        //% blockId="celsius" block="temperature (\u00B0\\C) on %pin|"
+        //% shim=dstemp::celsius
+        //% parts=dstemp trackArgs=0
+        export function celsius(pin: DigitalPin) : number {
+            return 32.6;
+        }
+
+        // Helper function
+        //% shim=dstemp::setErrorHandler
+        export function setErrorHandler(a: Action) {
+            errorHandler = a;
+        }
+
+        // Helper function
+        //% shim=dstemp::getErrorObjectIdx
+        export function getErrorObjectIdx() : number {
+            return errorObjectIdx;
+        }
+
+        // Helper function
+        //% shim=dstemp::getErrorPort
+        export function getErrorPort() : number {
+            return errorPort;
+        }
+
+        /**
+         * Set a handler for errors
+         * @param errCallback The error handler
+         */
+        //% blockId="error" block="temperature sensor error"
+        //% draggableParameters="reporter" weight=0
+        export function sensorError(errCallback: (errorMessage: string, errorCode: number, port: number) => void) {
+            if(errCallback) {
+                errorHandler = () => {
+                    let i  = getErrorObjectIdx();
+                    let p = getErrorPort();
+                    errCallback(errorMsgs[i], i, p);
+                };
+            } else {
+                errorHandler = null;
+            }
+            setErrorHandler(errorHandler);
+        };
 }
